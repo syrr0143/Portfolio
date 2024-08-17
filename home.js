a2eb6363a0fb214695882ace580663b2c821955e
syrr0143 <sy816120@gmail.com>
2024-08-17 23:21:29 +0530

    updated

---
+++ home.js
@@ -0,0 +1,75 @@
+<!-- ![logo](https://github.com/sohail300/sohail300/blob/main/bannerv1.png) -->
+<h1 align="center">Hi 👋, I'm Md Sohail Ansari</h1>
+<h3 align="center">A passionate Full Stack Developer from India</h3>
+
+<img align="right" alt="Coding" width="240" src="https://cdn.dribbble.com/users/1162077/screenshots/3848914/programmer.gif">
+
+<!-- Intro -->
+- ⚡ Full Stack **Web Developer**
+- 👨‍💻 Proficient in **Java**
+- 🌎 **Open Source** Geek
+- 🔥 My [Leetcode](https://www.leetcode.com/sohail60) Profile
+- 📫 Reach me at **sohailatwork10@gmail.com**
+
+## Tech Stack
+### Libraries/Frameworks
+[![My Skills](https://skillicons.dev/icons?i=react,next,nodejs,express&perline=9)](https://skillicons.dev)
+
+### Languages
+[![My Skills](https://skillicons.dev/icons?i=js,ts,java,python,bash&perline=9)](https://skillicons.dev)
+
+### Databases
+[![My Skills](https://skillicons.dev/icons?i=mongodb,firebase,postgresql&perline=9)](https://skillicons.dev)
+
+### Tools
+[![My Skills](https://skillicons.dev/icons?i=linux,git,github,figma&perline=9)](https://skillicons.dev)
+
+### Deployments
+[![My Skills](https://skillicons.dev/icons?i=aws,azure,vercel&perline=9)](https://skillicons.dev)
+<br>
+
+## Experience
+### GLOCYBS
+
+<img align="right" alt="Coding" width="140" src="https://media.licdn.com/dms/image/D560BAQER4ZupFZauYA/company-logo_200_200/0/1689921303802/glocybs_logo?e=1719446400&v=beta&t=QdftmFzKMz053oJk4Hwsp-Wjxh0hJebQ1RhldJ46NoI">
+
+- ⚡ Developed a Document Generator using React and ExpressJS as an internal tool to automate generation of documents like Offer Letter, Non-Disclosure agreement, Bond etc. in the organization.
+- 👨‍💻 Currently developing their company website.
+- 🌎 Crafting scalable web applications and overseeing the optimization of their website.
+<br>
+
+### STREEYA
+
+- ⚡ Designed 50+ web pages for their website establishing a strong brand identity.
+- 👨‍💻 Crafted a responsive design strategy, ensuring a seamless user experience across various devices.
+<br>
+
+## Connect with me
+  <a href="https://twitter.com/sohail_twts">
+    <img width="30px" src="https://www.vectorlogo.zone/logos/twitter/twitter-official.svg" />
+  </a>&ensp;
+  <a href="https://www.linkedin.com/in/md-sohail-ansari-b51201278/">
+    <img width="30px" src="https://www.vectorlogo.zone/logos/linkedin/linkedin-icon.svg" />
+  </a>&ensp;
+  <a href="https://instagram.com/sohail_infinity">
+    <img width="30px" src="https://www.vectorlogo.zone/logos/instagram/instagram-icon.svg" />
+  </a>
+<br>
+<br>
+
+<!-- Languages Used -->
+<p align="left">
+<img src="https://github-readme-stats.vercel.app/api/top-langs?username=sohail300&show_icons=true&locale=en&layout=compact&theme=aura" alt="sohail300" width="350" />
+</p>
+
+<!-- Github stats -->
+<p align="center" >
+<img src="https://github-readme-stats.vercel.app/api?username=sohail300&theme=chartreuse-dark&include_all_commits=true" width="350">
+</p>
+
+<!-- Streak -->
+<p align="right">
+<img src="https://github-readme-streak-stats.herokuapp.com/?user=sohail300&theme=algolia" alt="sohail300" width="350" />
+</p>
+
+</br>
